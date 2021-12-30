c7eae5f7149892199e19900d5200e75d2baf27e0
MangeshIpperNCI <mangesh.ipper@gmail.com>
2021-12-30 22:24:40 +0000

    plyed with click functions

--- tutorial01/src/Content.js
+++ tutorial01/src/Content.js
@@ -3,10 +3,26 @@ const Content = () => {
         const names = ['Mangesh', 'Sangesh', 'Akshay']
         const int = Math.floor(Math.random() * 3)
         return names[int]
+      };
+
+      const handleClick = () => {
+        console.log("Just clicked")
       }
+      
+      const handleClick2 = (name) => {
+        console.log("Just clicked by", name)
+      }
+      
+      const handleClick3 = (e) => {
+        console.log("Just clicked by", e.target.innerText)
+      }
+
     return (
         <main>
             <h1>Hello {handleNameChange()}!</h1>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={() => handleClick2("Mangesh")}>Click it</button>
+            <button onClick={(e) => handleClick3(e)}>Mangesh</button>
         </main>
     )
 }
